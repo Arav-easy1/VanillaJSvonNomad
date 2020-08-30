2fbfdb288272024809fd5afc186733566400e505
EASY_ARAV <wg0418@gmail.com>
2020-08-30 19:20:37 +0900

    data type

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const a = 221;
-let b = a - 5;
-a = 4;
-console.log(b, a);
+// 변수는 웬만하면 const로 쓰고 필요시에만 let을 쓰자!
+
+/* String 
+const what = "ARAV";
+*/
+/*
+Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 666;
+*/
+/* 
+Float
+const wat = 55.1;
+*/
