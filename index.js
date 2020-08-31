eaa2da1ee1e92e32d9868d285b36882cba0da234
EASY_ARAV <wg0418@gmail.com>
2020-08-31 17:27:24 +0900

    DOM 2 - document.querySelector("#title");

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const title = document.getElementById("title");
+// const title = document.querySelector("#title");   // 노드의 첫번째 자식을 반환함. #id로 찾기 .class로 찾기
+const title = document.querySelector("#title");
 title.innerHTML = "Hi! From JS";
+title.style.color = 'red';
+document.title = "I own you now";
