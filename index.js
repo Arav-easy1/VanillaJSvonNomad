3882402686beb5afc13b257893180e287fdcb0f4
EASY_ARAV <wg0418@gmail.com>
2020-08-31 19:15:05 +0900

    DOM if else Function practice 2 - toggle

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#4b6584";
+const CLICKED_CLASS = "clicked";
+
+// function handleClick() {
+//   const hasClass = title.classList.contains(CLICKED_CLASS); // title의 class가 CLICKED_CLASS를 포함하고 있으면 true.
+//   if (hasClass) {
+//     title.classList.remove(CLICKED_CLASS);
+//   } else {
+//     title.classList.add(CLICKED_CLASS);
+//   }
+// }
+// 이 모든걸 줄여서 toggle 함수가 알아서 해줌^^!
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener("click", handleClick);
 }
 init();
